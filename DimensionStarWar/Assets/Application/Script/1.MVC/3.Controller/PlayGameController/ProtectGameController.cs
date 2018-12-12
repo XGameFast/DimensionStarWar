0107b0f0947ad46a88f57e87c50ac2f6b58547da
chenzhongda <x_game@aliyun.com>
2018-12-12 10:57:02 +0800

    提交

---
+++ DimensionStarWar/Assets/Application/Script/1.MVC/3.Controller/PlayGameController/ProtectGameController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtectGameController : BaseController {
+
+    #region 开始
+
+    public override void StartController()
+    {
+        base.StartController();
+    }
+
+    #endregion
+
+    #region 结束
+    #endregion
+}
