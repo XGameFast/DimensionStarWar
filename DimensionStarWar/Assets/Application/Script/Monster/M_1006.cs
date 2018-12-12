e5e07d9597ba275bac2fadb63597af4d3a7f49be
GeJinPeng <x_game_gjp@aliyun.com>
2018-12-12 19:00:24 +0800

    1006部分BUG修改 流程走通

--- DimensionStarWar/Assets/Application/Script/Monster/M_1006.cs
+++ DimensionStarWar/Assets/Application/Script/Monster/M_1006.cs
@@ -3,6 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class M_1006 : MonsterBasic {
+    public List<Renderer> renderers;
 
-	
+    public override void SetAlpha(float v)
+    {
+        base.SetAlpha(v);
+        //for (int i = 0; i < renderers.Count; i++)
+        //{
+        //    renderers[i].material.SetFloat("_alpha", v);
+        //}
+    }
+    public override void SetRongjie(float v)
+    {
+        base.SetRongjie(v);
+        for (int i = 0; i < renderers.Count; i++)
+        {
+            foreach (var m in renderers[i].materials)
+            {
+                m.SetFloat("_rongjie", v);
+            }
+          
+        }
+    }
 }
